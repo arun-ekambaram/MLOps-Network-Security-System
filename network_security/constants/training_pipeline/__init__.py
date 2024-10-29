8f49ef78eff8dd82a5ecc52ac84fd22b42b1318a
arun ekambaram <arunvit1730@gmail.com>
2024-10-29 16:06:40 +0530

    updated Constants

--- network_security/constants/training_pipeline/__init__.py
+++ network_security/constants/training_pipeline/__init__.py
@@ -47,7 +47,7 @@ Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
-
+## knn imputer to replace nan valueupda
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values":np.nan,
     "n_neighbors":3,
