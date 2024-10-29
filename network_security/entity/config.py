ed7db3a72428e9b295b9e3f8796895ddfc6fc445
arun ekambaram <arunvit1730@gmail.com>
2024-10-29 15:57:41 +0530

    updated Constants

--- network_security/entity/config.py
+++ network_security/entity/config.py
@@ -36,4 +36,9 @@ class DataValidationConfig:
         self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
         self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
         self.drift_report_file_path: str = os.path.join(self.data_validation_dir,training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
-        
+
+
+
+class DataTransformationConfig:
+    def __init__(self,training_pipiline_config: TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join(training_pipiline_config.artifact_dir,training_pipeline.Datatra)
