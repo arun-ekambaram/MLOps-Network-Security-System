605335ac6dca764d2b6b20ae737b16ad8a3c4a1f
arun ekambaram <arunvit1730@gmail.com>
2024-10-30 22:42:49 +0530

    Added Training Pipeline

--- network_security/pipeline/training_pipeline.py
+++ network_security/pipeline/training_pipeline.py
@@ -42,8 +42,31 @@ class TrainingPipeline:
             data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
             logging.info("Start the Data Validation")
             data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info(f"Data Validation Completed and artifact: {data_validation_artifact}")
             return data_validation_artifact
-
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def start_data_transformation(self,data_validation_artifact: DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+            logging.info("Start the Data Transformation")
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            logging.info(f"Data Transformation Completed and artifact: {data_transformation_artifact}")
+            return data_transformation_artifact
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def start_model_trainer(self,data_transformation_artifact: DataTransformationArtifact)-> ModelTrainerArtifact:
+        try:
+            self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,model_trainer_config=self.model_trainer_config)
+            logging.info("Start the Model Trainer")
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            logging.info(f"Model Trainer Completed and artifact: {model_trainer_artifact}")
+            return model_trainer_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
