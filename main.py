c605d846a083687d66d161714395302421a3138c
arun ekambaram <arunvit1730@gmail.com>
2024-10-30 19:05:54 +0530

    Model Trainer updated

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from network_security.components.data_ingestion import DataIngestion
 from network_security.components.data_validation import DataValidation
 from network_security.components.data_transformation import DataTransformation
+from network_security.components.model_trainer import ModelTrainer
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
+from network_security.entity.config import DataIngestionConfig, DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from network_security.entity.config import TrainingPipelineConfig
 import sys
 
@@ -29,5 +30,10 @@ if __name__ == '__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         logging.info(" Data Transformation Completed")
         print(data_transformation_artifact)
+        logging.info("Model Training Started")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info(" Model Training Completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
