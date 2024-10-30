5bb836ec227dbde28198e2edc9a7850785c4ae26
arun ekambaram <arunvit1730@gmail.com>
2024-10-30 16:04:03 +0530

    Model Trainer component updated

--- network_security/utils/main_utils/utils.py
+++ network_security/utils/main_utils/utils.py
@@ -48,3 +48,25 @@ def save_object(file_path: str,obj: object)-> None:
         logging.info("Exited the save_object method of main utils class")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+    
+def load_object(file_path:str,)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file:{file_path} is not exist")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_numpy_array_data(file_path:str)->np.array:
+    """
+    load numpy array data from fille
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
