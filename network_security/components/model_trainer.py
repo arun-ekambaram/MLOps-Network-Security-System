2e8aa6f2747be5deecac030bb28d579745d6cf07
arun ekambaram <arunvit1730@gmail.com>
2024-10-30 17:10:23 +0530

    Model Trainer component updated

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -7,6 +7,11 @@ from network_security.utils.ml_utils.model.estimator import NetworkModel
 from network_security.utils.main_utils.utils import save_object,load_object
 from network_security.utils.main_utils.utils import load_numpy_array_data
 from network_security.utils.ml_utils.metric.classification_metric import get_classification_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier,RandomForestClassifier
 
 
 class ModelTrainer:
@@ -17,7 +22,45 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def train_model(self,X_train,y_train):
+    def train_model(self,x_train,y_train,x_test,y_test):
+        models = {
+            "Random Forest": RandomForestClassifier(verbose=1),
+            "Decision Tree": DecisionTreeClassifier(),
+            "Gradient Boosting": GradientBoostingClassifier(verbose=1),
+            "Logistic Regression": LogisticRegression(verbose=1),
+            "AdaBoost": AdaBoostClassifier(),
+
+        }
+
+        params = {
+            "Decision Tree":{
+                'criterion':['gini','entropy','log_loss'],
+                # 'splitter': ['best','random'],
+                # 'max_features':['sqrt','log2'],
+            },
+            "Random Forest":{
+                # 'criterion':['gini','entropy','log_loss'],
+                # 'max_features':['sqrt','log2',None],
+                'n_estimators':[8,16,32,64,128,256]
+            },
+            "Gradient Boosting":{
+                # 'loss':['log_loss','exponential'],
+                'learning_rate': [.1,.01,.05,.001],
+                'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                # 'criterion':['squared_error','friedman_mse'],
+                # 'max_features':['auto','sqrt','log2'],
+                'n_estimators':[8,16,32,64,128,256]
+
+            },
+            "Logistic Regression":{},
+            "AdaBoost":{
+                'learning_rate':[.1,.01,0.5,.001],
+                'n_estimators':[8,16,32,64,128,256]
+            }
+
+        }
+
+
 
         
     def initiate_model_trainer(self)-> ModelTrainerArtifact:
