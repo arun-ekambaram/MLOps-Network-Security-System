669824f6f1d4500200275953902a082a5f70b9e9
arun ekambaram <arunvit1730@gmail.com>
2024-10-30 09:11:08 +0530

    updated data transformation

--- network_security/components/data_transformation.py
+++ network_security/components/data_transformation.py
@@ -10,3 +10,46 @@ from network_security.entity.config import DataTransformationConfig
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
 from network_security.utils.main_utils.utils import save_numpy_array_data,save_object
+
+
+class DataTransformation:
+    def __init__(self,data_validation_artifact: DataValidationArtifact,
+                 data_transformation_config: DataTransformationConfig):
+        try:
+            self.data_validation_artifact:DataValidationArtifact = data_validation_artifact
+            self.data_transformation_config:DataTransformationConfig = data_transformation_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    @staticmethod ## dont need to initialize any objects. directly with the help of class name we can call it
+    def read_data(file_path) ->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+    def initiate_data_transformation(self)-> DataTransformationArtifact:
+        logging.info("Entered the initiate_data_transformation method of Data Transformation class")
+        try:
+            logging.info("Starting data transformation")
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_dat(self.data_validation_artifact.valid_test_file_path)
+
+            ## training dataframe
+            ## independent features
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN],axis=1)
+            ## dependent feature
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1,0)
+
+            ## testing dataframe
+             ## independent features
+            input_feature_test_df  = test_df.drop(columns=[TARGET_COLUMN],axis=1)
+            ## dependent feature
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1,0)
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
