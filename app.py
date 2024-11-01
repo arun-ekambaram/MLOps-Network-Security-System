d4ca06e59dd3ff73b8f9522ab8420fb40244d8c8
arun ekambaram <arunvit1730@gmail.com>
2024-11-01 17:41:34 +0530

    Deployment changes

--- app.py
+++ app.py
@@ -24,11 +24,61 @@ from network_security.constants.training_pipeline import DATA_INGESTION_COLLECTI
 ## Read the client Mongo
 
 client = pymongo.MongoClient(mongo_db_url)
+# client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
 database = client[DATA_INGESTION_DATABASE_NAME]
 collection = database[DATA_INGESTION_COLLECTION_NAME]
 
 
+# app = FastAPI()
+# origins = ["*"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# from fastapi.templating import Jinja2Templates
+# templates = Jinja2Templates(directory="./templates")
+
+# @app.get("/", tags=["authentication"])
+# async def index():
+#     return RedirectResponse(url="/docs")
+
+# @app.get("/train")
+# async def train_route():
+#     try:
+#         train_pipeline=TrainingPipeline()
+#         train_pipeline.run_pipeline()
+#         return Response("Training is successful")
+#     except Exception as e:
+#         raise NetworkSecurityException(e,sys)
+    
+# @app.post("/predict")
+# async def predict_route(request: Request,file: UploadFile = File(...)):
+#     try:
+#         df=pd.read_csv(file.file)
+#         preprocesor=load_object("final_model/preprocessor.pkl")
+#         final_model=load_object("final_model/model.pkl")
+#         network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
+#         print(df.iloc[0])
+#         y_pred = network_model.predict(df)
+#         print(y_pred)
+#         df['predicted_column'] = y_pred
+#         print(df['predicted_column'])
+#         df.to_csv('prediction_output/output.csv')
+#         table_html = df.to_html(classes='table table-striped')
+#         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+#     except Exception as e:
+#             raise NetworkSecurityException(e,sys)
+
+    
+# if __name__=="__main__":
+#     app_run(app,host="0.0.0.0",port=8000)
 app = FastAPI()
 origins = ["*"]
 
@@ -60,6 +110,7 @@ async def train_route():
 async def predict_route(request: Request,file: UploadFile = File(...)):
     try:
         df=pd.read_csv(file.file)
+        #print(df)
         preprocesor=load_object("final_model/preprocessor.pkl")
         final_model=load_object("final_model/model.pkl")
         network_model = NetworkModel(preprocessor=preprocesor,model=final_model)
@@ -68,8 +119,11 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         print(y_pred)
         df['predicted_column'] = y_pred
         print(df['predicted_column'])
+        #df['predicted_column'].replace(-1, 0)
+        #return df.to_json()
         df.to_csv('prediction_output/output.csv')
         table_html = df.to_html(classes='table table-striped')
+        #print(table_html)
         return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
         
     except Exception as e:
